789a68cc733b9adf3106c321d0b379c506dd983b
appinha <amanda_pinha@hotmail.com>
2024-11-15 20:54:25 -0300

    configure eslint-plugin-eslint-comments

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
+import eslintComments from "eslint-plugin-eslint-comments";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
@@ -30,12 +31,16 @@ export default tseslint.config(
       ...react.configs.flat.recommended.languageOptions,
     },
     plugins: {
+      "eslint-comments": eslintComments,
       react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     settings: { react: { version: "detect" } },
     rules: {
+      ...eslintComments.configs.recommended.rules,
+      "eslint-comments/require-description": "error",
+
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": [
         "warn",
