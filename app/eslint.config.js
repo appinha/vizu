a04b62f2f28923fae57abd5b9697991ac4ee4333
appinha <amanda_pinha@hotmail.com>
2024-11-15 20:30:25 -0300

    configure eslint-plugin-react

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
@@ -8,16 +9,29 @@ import prettier from "eslint-config-prettier";
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettier,
+      react.configs.flat.recommended,
+      react.configs.flat["jsx-runtime"],
+    ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+      parserOptions: { ecmaFeatures: { jsx: true } },
+      ...react.configs.flat.recommended.languageOptions,
     },
     plugins: {
+      react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
+    settings: { react: { version: "detect" } },
     rules: {
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": [
