18d5a8ba314c45aab78974a2aa0d6fad34639809
Cauê Bittencourt Carnietto <cbcaue@protonmail.com>
2024-11-16 11:02:40 -0300

    Add eslint-plugin-simple-import-sort (#6)

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -9,6 +9,7 @@ import prettier from "eslint-config-prettier";
 import vitest from "@vitest/eslint-plugin";
 import jestDom from "eslint-plugin-jest-dom";
 import testingLibrary from "eslint-plugin-testing-library";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -31,6 +32,7 @@ export default tseslint.config(
       ...react.configs.flat.recommended.languageOptions,
     },
     plugins: {
+      "simple-import-sort": simpleImportSort,
       "eslint-comments": eslintComments,
       react,
       "react-hooks": reactHooks,
@@ -40,6 +42,9 @@ export default tseslint.config(
     rules: {
       ...eslintComments.configs.recommended.rules,
       "eslint-comments/require-description": "error",
+
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
 
       ...reactHooks.configs.recommended.rules,
       "react/no-array-index-key": "error",
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,7 +3,7 @@ import Page from "@/components/Page";
 
 export default function App() {
   return (
-    <div className="min-h-screen w-full relative flex flex-col bg-background m-0 p-0">
+    <div className="relative m-0 flex min-h-screen w-full flex-col bg-background p-0">
       <Navbar />
       <Page className="h-[calc(100vh-4rem)]" />
     </div>
