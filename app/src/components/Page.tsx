8b5319e20e68cb187d86d5ba5b3fae835e5fa2ef
cacauisadog <cbcaue@protonmail.com>
2024-12-10 20:27:11 -0300

    create example `Ping backend` button just for testing purposes

--- app/src/components/Page.tsx
+++ app/src/components/Page.tsx
@@ -1,7 +1,15 @@
+import { useCallback } from "react";
+
 import logo from "/logo-lines.svg";
+import api from "@/api";
 import { cn } from "@/lib/utils";
 
 export default function Page({ className }: { className?: string }) {
+  const pingBackend = useCallback(async () => {
+    const data = await api.healthcheck();
+    console.log(data);
+  }, []);
+
   return (
     <main className="max-w-xxl mx-auto flex-1">
       <div
@@ -12,6 +20,7 @@ export default function Page({ className }: { className?: string }) {
         )}
       >
         <img src={logo} className="h-36" alt="Vizu logo" />
+        <button onClick={pingBackend}>Ping backend</button>
       </div>
     </main>
   );
