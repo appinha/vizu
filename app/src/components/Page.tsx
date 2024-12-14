650ebffd317ac078e6116e7ab465fb2a979e6a9f
cacauisadog <cbcaue@protonmail.com>
2024-12-14 09:45:51 -0300

    Revert "create example `Ping backend` button just for testing purposes"
    This reverts commit 8b5319e20e68cb187d86d5ba5b3fae835e5fa2ef.

--- app/src/components/Page.tsx
+++ app/src/components/Page.tsx
@@ -1,15 +1,7 @@
-import { useCallback } from "react";
-
 import logo from "/logo-lines.svg";
-import api from "@/api";
 import { cn } from "@/lib/utils";
 
 export default function Page({ className }: { className?: string }) {
-  const pingBackend = useCallback(async () => {
-    const data = await api.healthcheck();
-    console.log(data);
-  }, []);
-
   return (
     <main className="max-w-xxl mx-auto flex-1">
       <div
@@ -20,7 +12,6 @@ export default function Page({ className }: { className?: string }) {
         )}
       >
         <img src={logo} className="h-36" alt="Vizu logo" />
-        <button onClick={pingBackend}>Ping backend</button>
       </div>
     </main>
   );
