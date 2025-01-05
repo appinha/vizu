dea4d2ec6f0a814dbf52f3cbb15cdc5abc33203d
Amanda P. Pinha <49294747+appinha@users.noreply.github.com>
2025-01-05 11:21:37 -0300

    feat: expenses crud (#24)
    Co-authored-by: cacauisadog <cbcaue@protonmail.com>

--- backend/vizu/apps/core/views.py
+++ backend/vizu/apps/core/views.py
@@ -1,7 +1,11 @@
-from django.http import JsonResponse
-from ninja import NinjaAPI, Schema
+from datetime import date as Idate
 from decimal import Decimal
-from typing import List
+
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from ninja import NinjaAPI, Schema
+
+from vizu.apps.core.models import Expense
 
 api = NinjaAPI()
 
@@ -11,26 +15,63 @@ def healthcheck(request):
     return JsonResponse({"status": "ok"})
 
 
-class ExpenseOut(Schema):
-    date: str
+class ExpenseGet(Schema):
+    id: int
+    date: Idate
     value: Decimal
     description: str
     category: str
 
 
-@api.get("/expenses/", response=List[ExpenseOut])
+@api.get("/expenses/", response=list[ExpenseGet])
 def list_expenses(request):
-    return [
-        {
-            "date": "2024-12-10",
-            "value": 81.5,
-            "description": "Mia Pizzaria",
-            "category": "Restaurante",
-        },
-        {
-            "date": "2024-12-07",
-            "value": 288,
-            "description": "Risoteria Villa Lobos",
-            "category": "Restaurante",
-        },
-    ]
+    return Expense.objects.all()
+
+
+@api.get("/expenses/{id}/", response=ExpenseGet)
+def get_expense(request, id: int):
+    return get_object_or_404(Expense, id=id)
+
+
+class ExpenseCreate(Schema):
+    date: Idate
+    value: Decimal
+    description: str = None
+    category: str = None
+
+
+@api.post("/expenses/")
+def create_expense(request, payload: ExpenseCreate):
+    expense = Expense.objects.create(**payload.dict())
+    return JsonResponse(
+        {"data": expense.to_dict(), "message": "Expense created successfully."},
+        status=201,
+    )
+
+
+class ExpenseUpdate(Schema):
+    date: Idate = None
+    value: Decimal = None
+    description: str = None
+    category: str = None
+
+
+@api.put("/expenses/{id}/")
+def update_expense(request, id: int, payload: ExpenseUpdate):
+    expense = get_object_or_404(Expense, id=id)
+
+    for attr, value in payload.dict(exclude_unset=True).items():
+        setattr(expense, attr, value)
+
+    expense.save()
+
+    return JsonResponse(
+        {"data": expense.to_dict(), "message": "Expense updated successfully."}
+    )
+
+
+@api.delete("/expenses/{id}/")
+def delete_expense(request, id: int):
+    expense = get_object_or_404(Expense, id=id)
+    expense.delete()
+    return {"message": "Expense deleted successfully."}
