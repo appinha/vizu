f097cc6390fa4c3bb37c9bb0125990095589cd7a
appinha <amanda_pinha@hotmail.com>
2025-02-02 12:06:59 -0300

    delete unused api method

--- app/src/api/index.ts
+++ app/src/api/index.ts
@@ -1,15 +1,6 @@
 import { get } from "@/api/fetchUtils";
 
 export default {
-  async healthcheck() {
-    console.log("pinging backend");
-    const data = await get("/api/_healthcheck/");
-
-    if (!data) return { status: "error" };
-
-    return await data.json();
-  },
-
   async getExpenses() {
     const data = await get("/api/expenses/");
 
